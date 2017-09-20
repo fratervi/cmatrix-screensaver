09b18c0351789a0699d2611ffa85de65a23e031d
fratervi <32000047+fratervi@users.noreply.github.com>
2017-09-20 15:43:07 -0700

    improved process ID and timing management.

--- matrix.sh
+++ matrix.sh
@@ -8,30 +8,41 @@ sesh="pts/$(echo $SSH_TTY |cut -d'/' -f4)" #identifies current pts terminal sess
 while true
         do
         idle=$(w |grep "${sesh}" | tr -s " " | cut -d" " -f5 |cut -d "." -f1) #acquires current session idle time
-        if [ $(echo "${idle}" | grep ':') ] ; then #if idle time is more than a minute converts idle time from minute:seconds to seconds
-                idle=$(echo $idle | awk -F: '{ print ($1 * 60) + $2 }')
+        if [ $(echo "${idle}" | grep ':') ] ; then #if idle time is more than a minute converts idle time from minute:seconds to seconds or hours:minutes to seconds
+                time1=$(echo $idle | awk -F: '{ print ($1)}')
+                time2=$(echo $idle | awk -F: '{ print ($2)}')
+                if [ $(echo "${idle}" | grep 'm') ] ; then #drops m from ##:##m format for idle times greater than 1 hour then calculates seconds
+                        time2=$(echo "$time2" | cut -d"m" -f1)
+                        idle=$(((time1 * 3600)+(time2 * 60))) 
+                else
+                        time2=`echo $time2|sed 's/^0*//'` #drops leading zero from seconds on minute:seconds format to avoid the seconds from being read as an octal value
+                        idle=$(((time1 * 60)+time2))
+                fi
         fi
-#       clear #can uncomment for trouble shooting
-#       echo $idle #can uncomment for trouble shooting
-#       echo $sesh #can uncomment for trouble shooting
-        ps -a | grep -v grep | grep cmatrix > /dev/null #determines if cmatrix is already running I need to add more to make sure it is the current sessions process
+#       clear
+#       echo $idle
+#       echo $sesh
+        ps -a | grep -v grep | grep ${sesh} | grep cmatrix > /dev/null #checks for cmatrix in running processes
         result=$?
-#       echo $result #can uncomment for trouble shooting
-        if [ "${idle}" -gt "300" ] ; then #if idle time is greater than 300 check for cmatrix and if it is not running launch it
+#       echo $result
+        if [ "${idle}" -gt "300" ] ; then #checks idles time and kicks off cmatrix if exceeds 300 seconds
                 if [ "${result}" -eq "0" ] ; then
                         echo "still running"  > /dev/null
                 else
                         echo "not running" > /dev/null
-                        /usr/bin/cmatrix -bau 2 &
+                        /usr/bin/cmatrix -bau 2 -C magenta &
                 fi
         else
                 echo "not idle long enough"  > /dev/null
         fi
-        if [ "${idle}" -lt "300" ] ; then #if idle time is less than 300 check for cmatrix and if it is running kill it
+        if [ "${idle}" -lt "300" ] ; then #checks idle time and kills cmatrix if it drops below 300
                 if [ "${result}" -eq "0" ] ; then
                         echo "still running"  > /dev/null
                         # echo -e "q\n"
-                        /usr/bin/killall -9 cmatrix > /dev/null
+                        # /usr/bin/killall -9 cmatrix > /dev/null
+                        process=$(ps -aux | grep -i "${sesh}" | grep -i cmatrix | cut -d " " -f2,3)
+                        /usr/bin/kill -9 $process
+                        clear
                 else
                         echo "not running" > /dev/null
                 fi
