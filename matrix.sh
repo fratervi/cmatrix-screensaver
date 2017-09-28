e9d8fa9d22e79f9766d5aca6da9ad2b2a231c03f
fratervi <32000047+fratervi@users.noreply.github.com>
2017-09-28 14:51:38 -0700

    Update matrix.sh

--- matrix.sh
+++ matrix.sh
@@ -22,17 +22,17 @@ while true
                 else
                         time2=`echo $time2|sed 's/^0*//'` #drops leading zero from seconds on minute:seconds format to avoid the seconds from being read as an octal value
                         idle=$(((time1 * 60)+time2))
-						echo "New Idle time = ${idle}" >> ~/bin/matrix.log
+			echo "New Idle time = ${idle}" >> ~/bin/matrix.log
                 fi
        fi
        echo "idle time = ${idle}" >> ~/bin/matrix.log
        echo "session = $sesh" >> ~/bin/matrix.log
-        ps -a | grep -v grep | grep ${sesh} | grep cmatrix > /dev/null #checks for cmatrix in running processes
-        result=$?
+       ps -a | grep -v grep | grep ${sesh} | grep cmatrix > /dev/null #checks for cmatrix in running processes
+       result=$?
        echo "result = ${result}" >> ~/bin/matrix.log
-        if [ "${idle}" -gt "30" ] ; then #checks idles time and kicks off cmatrix if exceeds 300 seconds
-                if [ "${result}" -eq "0" ] ; then
-                        echo "cmatrix still running"  >> ~/bin/matrix.log
+       if [ "${idle}" -gt "30" ] ; then #checks idles time and kicks off cmatrix if exceeds 300 seconds
+       		if [ "${result}" -eq "0" ] ; then
+                echo "cmatrix still running"  >> ~/bin/matrix.log
                 else
                         echo "cmatrix not running, starting" >> ~/bin/matrix.log
                         /usr/bin/cmatrix -bau 2 -C magenta &
@@ -42,8 +42,8 @@ while true
         fi
         if [ "${idle}" -lt "30" ] ; then #checks idle time and kills cmatrix if it drops below 300
                 if [ "${result}" -eq "0" ] ; then
-                        echo "cmatrix still running, identifying and killing it"  >> ~/bin/matrix.log
-                        # echo -e "q\n"
+                	echo "cmatrix still running, identifying and killing it"  >> ~/bin/matrix.log
+                       	# echo -e "q\n"
                         # /usr/bin/killall -9 cmatrix > /dev/null
 			 process=$(ps -aux | grep -i "${sesh}" | grep -i cmatrix | tr -s "  " |  cut -d " " -f2)
 			echo "$process - PID $(ps -aux | grep -i "${sesh}" | grep -i cmatrix)" >> ~/bin/matrix.log
